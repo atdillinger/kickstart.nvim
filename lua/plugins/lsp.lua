0288fb8cb6fb86442d7ccc99684795141c59ec14
Austin <shuffletick@gmail.com>
2024-10-14 14:02:21 -0600

    add python support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -142,7 +142,7 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -176,7 +176,6 @@ return {
       --
       --  You can press `g?` for help in this menu.
       require('mason').setup()
-
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
