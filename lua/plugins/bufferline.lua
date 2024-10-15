6fb6d00ab1d5f255d6248a56c650b1cba52b06f0
Austin <shuffletick@gmail.com>
2024-10-15 11:28:21 -0600

    clean up buffer commands

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,8 +3,7 @@ return {
     'akinsho/bufferline.nvim',
     event = 'VeryLazy',
     keys = {
-      { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
-      { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete Non-Pinned Buffers' },
+      { '<leader>bd', '<Cmd>bd<CR>', desc = 'Delete Buffer' },
       { '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', desc = 'Delete Other Buffers' },
       { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
       { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
